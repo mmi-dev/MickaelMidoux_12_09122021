8683d546817b9518e98982c935885b844baacc12
mmi-dev <m.midoux@hotmail.fr>
2022-11-14 16:46:49 +0100

    eslint & prettier base config

--- tmp/app.js
+++ tmp/app.js
@@ -4,12 +4,25 @@ console.log("ok")
 const apiUrl = 'http://localhost:3001'
 const getUrl = apiUrl + '/user/12/activity'
 
-const containt = fetch(getUrl)
-  .then((response) => response.json())
-  .then((data) => data);
+async function getData() {
+    await fetch(getUrl)
+    .then(response => response.json())
+    .then(data => {
+        // const mom = JSON.stringify(data)
+        const mom = data
+        console.log(data.data.userId)
+        console.log(mom.data)
+
+
+        const html = document.createElement('div')
+        html.innerHTML= mom.data.userId
+
+    })
+    .catch(error => console.log(error));
+}
+
+getData()
 
 const page =  document.getElementById('root')
 
-console.log(containt)
-
-page.appendChild.containt
+page.appendChild.html
