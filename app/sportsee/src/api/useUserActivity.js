ede5a767adb4c7a77f854b4a463ef4a0fc0ca81d
mmi-dev <m.midoux@hotmail.fr>
2022-11-23 15:45:18 +0100

    story#6 - activity data

---
+++ app/sportsee/src/api/useUserActivity.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useContext } from 'react';
+import Auth from '../contexts/AuthContext';
+import UserData from '../contexts/UserDataContext';
+
+function useUserActivity(timeout = 0) {
+  const { userActivityData, setUserActivityData } = useContext(UserData);
+  const { userId } = useContext(Auth);
+  const [fetchError, setFetchError] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const API_URL = 'http://localhost:3001/user/' + userId + '/activity';
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const datas = await response.json();
+        setUserActivityData(datas);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    // simulate awaiting server response
+    setTimeout(() => {
+      (async () => await fetchApi())();
+    }, timeout);
+  }, []);
+
+  return { data: userActivityData, error: fetchError, loading: isLoading };
+}
+
+export default useUserActivity;
--- app/sportsee/src/pages/User.js
+++ app/sportsee/src/pages/User.js
@@ -3,12 +3,11 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import Auth from '../contexts/AuthContext';
 import UserData from '../contexts/UserDataContext';
 import useUserDetails from '../api/useUserDetails';
+import useUserActivity from '../api/useUserActivity';
 import SimpleLoader from '../components/loader/SimpleLoader';
 
 const User = () => {
   const { isAuthenticated } = useContext(Auth);
-
-  const userDetails = useUserDetails(3000);
 
   const {
     userDetailsData,
@@ -17,13 +16,31 @@ const User = () => {
     userSessionsData,
   } = useContext(UserData);
 
+  //user details
+  const userDetails = useUserDetails(2000);
   const [firstName, setFirstName] = useState('');
-
   useEffect(() => {
     if (userDetailsData.data) {
       setFirstName(userDetailsData.data.userInfos.firstName);
     }
   }, [userDetailsData]);
+
+  //user activity
+  const userActivity = useUserActivity();
+  const [dailyActivity, setDailyActivity] = useState('');
+  useEffect(() => {
+    if (userActivityData.data) {
+      setDailyActivity(userActivityData.data.sessions);
+      console.log(dailyActivity);
+      console.log(
+        dailyActivity ? dailyActivity.map((e) => e.kilogram) : 'nothing'
+      );
+    }
+  }, [userActivityData]);
+
+  // user average sessions
+
+  // user performance
 
   // category
   const [searchParams] = useSearchParams();
@@ -46,6 +63,24 @@ const User = () => {
             Bonjour <span className="firstname">{firstName}</span>{' '}
           </h1>
           <p>{category}</p>
+          <section>
+            <h2>activity</h2>
+            {userActivity.loading && <SimpleLoader />}
+            {!userActivity.loading && (
+              <div>
+                {dailyActivity
+                  ? dailyActivity.map((activity, i) => {
+                      return (
+                        <li key={i}>
+                          {activity.day}_{activity.kilogram}'Kgs'-
+                          {activity.calories}'calories'
+                        </li>
+                      );
+                    })
+                  : 'no data'}
+              </div>
+            )}
+          </section>
         </>
       )}
     </main>
